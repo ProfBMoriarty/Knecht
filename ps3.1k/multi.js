1189fc53960d284799d2146d1b37d460e7d8d386
CainToad <caintoad716@gmail.com>
2014-03-03 11:54:21 -0500

    Fixes (once more with feeling)

--- ps3.1k/multi.js
+++ ps3.1k/multi.js
@@ -70,6 +70,8 @@ function beHost()
     });
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 
     K.grantPermission("m_test", "caintoad@yahoo.com", "mult_test", function(response){
         if(response.result === K.OK){
@@ -100,13 +102,25 @@ function beHost()
 >>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
 =======
 >>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
 }
 
 function beUser(){
     //swagfish
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
     /*var to_read;
+=======
+    var to_read;
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
+    var to_read;
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
 =======
     var to_read;
 >>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
@@ -127,9 +141,21 @@ function beUser(){
     to_read.send();
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
     PS.statusText(text_to_read);*/
     position = "client";
     K.setAddress("http://localhost:8080");
+=======
+    PS.statusText(text_to_read);
+    position = "client";
+    K.setAddress(text_to_read + 8080);
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
+    PS.statusText(text_to_read);
+    position = "client";
+    K.setAddress(text_to_read + 8080);
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
 =======
     PS.statusText(text_to_read);
     position = "client";
@@ -148,6 +174,8 @@ function beUser(){
             PS.statusText("You are the client.");
         }
     });
+<<<<<<< HEAD
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 
@@ -170,6 +198,10 @@ function beUser(){
             }
         });
     });
+=======
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
 =======
 >>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
 =======
@@ -325,6 +357,8 @@ PS.keyDown = function( key, shift, ctrl, options ) {
     if(has_chosen){
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
         PS.glyph(user_x, user_y, " ");
         if(key == PS.KEY_ARROW_DOWN){
             if(user_y < 7){
@@ -354,12 +388,22 @@ PS.keyDown = function( key, shift, ctrl, options ) {
 =======
 =======
 >>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
         if(key == PS.KEY_ARROW_DOWN){
         }
         if(key == PS.KEY_ARROW_UP){}
         if(key == PS.KEY_ARROW_LEFT){}
         if(key == PS.KEY_ARROW_RIGHT){}
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
+>>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
+=======
 >>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
 =======
 >>>>>>> 6cbd25890ba74ea7b4c82cb637488c894d012595
