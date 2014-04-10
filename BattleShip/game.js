baf3f088b6593eb2774aac1ecebf8e232ddf84a1
pfeeney <p.feeney@wpi.edu>
2014-04-10 11:27:24 -0400

    crude version of battleship demo

--- BattleShip/game.js
+++ BattleShip/game.js
@@ -26,14 +26,46 @@ along with Perlenspiel. If not, see <http://www.gnu.org/licenses/>.
 
 // This is a template for creating new Perlenspiel games
 
-var field_x = 6;
-var field_y = 1;
-var s_select = null;
-var p_view = new Array(10);
-for(var i=0; i<10; i+=1){
-    p_view[i] = new Array(10);
+var board_x = 6; //left offset of board
+var board_y = 1; //top offset of board
+var board_dim = 10; //length of board side
+
+var own_ships =
+    [
+        {orientation: "hor", pos: {x: 18, y: 1}, color: PS.COLOR_GRAY_DARK, hits:[false, false, false]},
+        {orientation: "vert", pos: {x: 18, y: 3}, color: PS.COLOR_GRAY_DARK, hits:[false, false]},
+        {orientation: "vert", pos: {x: 18, y: 6}, color: PS.COLOR_GRAY_DARK, hits:[false, false, false, false, false]},
+        {orientation: "vert", pos: {x: 20, y: 3}, color: PS.COLOR_GRAY_DARK, hits:[false, false, false]},
+        {orientation: "vert", pos: {x: 20, y: 7}, color: PS.COLOR_GRAY_DARK, hits:[false, false, false, false]}
+    ];
+var opp_ships = //only known to host application, should not be displayed to user
+    [
+        {orientation: "hor", pos: {x: 18, y: 1}, color: PS.COLOR_GRAY_DARK, hits:[false, false, false]},
+        {orientation: "vert", pos: {x: 18, y: 3}, color: PS.COLOR_GRAY_DARK, hits:[false, false]},
+        {orientation: "vert", pos: {x: 18, y: 6}, color: PS.COLOR_GRAY_DARK, hits:[false, false, false, false, false]},
+        {orientation: "vert", pos: {x: 20, y: 3}, color: PS.COLOR_GRAY_DARK, hits:[false, false, false]},
+        {orientation: "vert", pos: {x: 20, y: 7}, color: PS.COLOR_GRAY_DARK, hits:[false, false, false, false]}
+    ];
+
+var hitboard = new Array(board_dim);
+for(var i = 0; i < board_dim; i++)
+{
+    hitboard[i] = new Array(board_dim);
 }
 
+var selected_piece = null;
+var num_placed = 0;
+
+var own_score = 0;
+var opp_score = 0;
+
+var player = null;
+var step = "unconnected";
+var setup_done = false;
+var other_setup = false;
+var view_own_ships = true;
+
+//changes all beads of color a contiguous with bead x, y to color b
 function flood(x, y, color_a, color_b){
     if(PS.color(x, y) == color_a){
         PS.color(x, y, color_b);
@@ -45,9 +77,10 @@ function flood(x, y, color_a, color_b){
     }
 }
 
-function within_field(x, y){
-    if((x >= field_x) && ( x< (field_x + 10))){
-        if((y >= field_y) && (y < (field_y + 10))){
+//returns true if coordinates are on board
+function on_board(x, y){
+    if((x >= board_x) && ( x< (board_x + board_dim))){
+        if((y >= board_y) && (y < (board_y + board_dim))){
             return true;
         }
         else{
@@ -59,35 +92,106 @@ function within_field(x, y){
     }
 }
 
-function check_collide(x, y, orient, size, left){
-    if(orient == "vert"){
-        if(PS.color(x, y) == PS.COLOR_CYAN){
-            if(left < (size - 1)){
-                return check_collide(x, y+1, orient, size, left+1);
-            }
-            else{
-                return true;
-            }
+//returns true if the selected piece can be placed in coordinate x,y, false if it collides with other piece or board edge
+function valid_placement(x, y, piece, cell_num)
+{
+    if(!cell_num)
+    {
+        cell_num = 1;
+    }
+    var hor_step = 0;
+    var vert_step = 0;
+    if(piece.orientation === "vert")
+    {
+        vert_step = 1;
+    }
+    else
+    {
+        hor_step = 1;
+    }
+    if(on_board(x, y) && PS.data(x, y) === 0)
+    { //if empty coord on board
+        if(cell_num < piece.hits.length)
+        {
+            return valid_placement(x + hor_step, y + vert_step, piece, cell_num + 1); //check the next bead in line
         }
-        else{
-            return false;
+        else return true; //last bead checked, all clear
+    }
+    else
+    {
+        PS.statusText("Fails at " + x + "," + y);
+        return false;//not a free space
+    }
+}
+
+function has_border(x, y){
+    return PS.border(x, y, PS.CURRENT);
+}
+
+function draw_board()
+{
+    //clear screen
+    PS.color(PS.ALL, PS.ALL, PS.COLOR_WHITE);
+    //board spaces
+    PS.applyRect(board_x, board_y, board_dim, board_dim, PS.color, PS.COLOR_CYAN);
+    //board border
+    PS.applyRect(board_x - 1, board_y - 1, board_dim + 2, 1, PS.color, PS.COLOR_BLACK);
+    PS.applyRect(board_x - 1, board_y - 1, 1, board_dim + 2, PS.color, PS.COLOR_BLACK);
+    PS.applyRect(board_x + board_dim, board_y - 1, 1, board_dim + 2, PS.color, PS.COLOR_BLACK);
+    PS.applyRect(board_x - 1, board_y + board_dim, board_dim + 2, 1, PS.color, PS.COLOR_BLACK);
+    //buttons
+    PS.applyRect(1, 1, 3, 1, PS.color, PS.COLOR_RED); //toggle between view of own and enemy ships
+    PS.applyRect(1, 3, 3, 1, PS.color, PS.COLOR_GREEN); //makes status line take an input message
+    PS.applyRect(1, 5, 3, 1, PS.color, PS.COLOR_BLACK); //logs in. If game doesnt exist, create it, otherwise join it
+
+    if(view_own_ships)
+    {
+        //ships
+        for(var i = 0; i < own_ships.length; i++)
+        {
+            if(own_ships[i].orientation === "hor")
+            {
+                PS.applyRect(own_ships[i].pos.x, own_ships[i].pos.y, own_ships[i].hits.length, 1, PS.color, own_ships[i].color);
+            }
+            else
+            {
+                PS.applyRect(own_ships[i].pos.x, own_ships[i].pos.y, 1, own_ships[i].hits.length, PS.color, own_ships[i].color);
+            }
         }
     }
-    if(orient == "horiz"){
-        if(PS.color(x, y) == PS.COLOR_CYAN){
-            if(left < (size - 1)){
-                return check_collide(x+1, y, orient, size, left+1);
+    else
+    {
+        for(var i = 0; i < board_dim; i++)
+        {
+            for(var j = 0; j < board_dim; j++)
+            {
+                if(hitboard[i][j] === false)
+                {
+                    PS.color(board_x + i, board_y + j, PS.COLOR_WHITE);
+                }
+                else if(hitboard[i][j] === true)
+                {
+                    PS.color(board_x + i, board_y + j, PS.COLOR_ORANGE);
+                }
             }
-            else{
-                return true;
-            }
-        }
-        else{
-            return false;
         }
     }
-    //this should never happen
-    return false;
+}
+
+function update_data()
+{
+    PS.data(PS.ALL, PS.ALL, 0);
+    for(var i = 0; i < own_ships.length; i++)
+    {
+        if(own_ships[i].orientation === "hor")
+        {
+            PS.applyRect(own_ships[i].pos.x, own_ships[i].pos.y, own_ships[i].hits.length, 1, PS.data, {index:i});
+        }
+        else
+        {
+            PS.applyRect(own_ships[i].pos.x, own_ships[i].pos.y, 1, own_ships[i].hits.length, PS.data, {index:i});
+        }
+    }
 }
 
 // All of the functions below MUST exist, or the engine will complain!
@@ -109,48 +213,23 @@ PS.init = function( system, options ) {
 	// Do this FIRST to avoid problems!
 	// Otherwise you will get the default 8x8 grid
 
-	PS.gridSize( 22, 12 );
+	K.setAddress("http://localhost:8080");
+    K.setApplication("battleship");
+    K.setErrorCallback(function(fn, err)
+    {
+        PS.statusText("Error processing " + fn + ": " + err);
+    });
+
+    PS.gridSize( 22, 12 );
+    //set up borders
     PS.border(PS.ALL, PS.ALL, 0);
-    //The Playing field
-    PS.applyRect(field_x, field_y, 10, 10, PS.color, PS.COLOR_CYAN);
-    PS.applyRect(field_x, field_y, 10, 10, PS.border, PS.DEFAULT);
+    PS.applyRect(board_x, board_y, board_dim, board_dim, PS.border, PS.DEFAULT);
 
-    for(var i=0; i<10; i += 1){
-        for(var j=0; j<10; j += 1){
-            p_view[i][j] = PS.COLOR_CYAN;
-        }
-    }
+    //draw initial board position
+    update_data();
+    draw_board();
 
-    //the border
-    PS.applyRect(5, 0, 12, 1, PS.color, PS.COLOR_BLACK);
-    PS.applyRect(5, 0, 1, 12, PS.color, PS.COLOR_BLACK);
-    PS.applyRect(16, 0, 1, 12, PS.color, PS.COLOR_BLACK);
-    PS.applyRect(5, 11, 12, 1, PS.color, PS.COLOR_BLACK);
-
-    //The buttons
-    PS.applyRect(1, 1, 3, 1, PS.color, PS.COLOR_RED);
-
-    PS.applyRect(1, 3, 3, 1, PS.color, PS.COLOR_GREEN);
-
-    PS.applyRect(1, 5, 3, 1, PS.color, PS.COLOR_BLACK);
-
-
-    //The ships
-    PS.applyRect(18, 1, 3, 1, PS.color, PS.COLOR_GRAY_DARK);
-    PS.applyRect(18, 1, 3, 1, PS.data, {"S": 1, "H": 3, "I": "horiz"});
-
-    PS.applyRect(18, 3, 1, 2, PS.color, PS.COLOR_GRAY_DARK);
-    PS.applyRect(18, 3, 1, 2, PS.data, {"S": 2, "H": 2, "I": "vert"});
-
-    PS.applyRect(18, 6, 1, 5, PS.color, PS.COLOR_GRAY_DARK);
-    PS.applyRect(18, 6, 1, 5, PS.data, {"S": 3, "H": 5, "I": "vert"});
-
-    PS.applyRect(20, 3, 1, 3, PS.color, PS.COLOR_GRAY_DARK);
-    PS.applyRect(20, 3, 1, 3, PS.data, {"S": 4, "H": 3, "I": "vert"});
-
-    PS.applyRect(20, 7, 1, 4, PS.color, PS.COLOR_GRAY_DARK);
-    PS.applyRect(20, 7, 1, 4, PS.data, {"S": 5, "H": 4, "I": "vert"});
-	// Add any other initialization code you need here
+    PS.statusText("Click to register and start/join game");
 };
 
 // PS.touch ( x, y, data, options )
@@ -169,25 +248,130 @@ PS.touch = function( x, y, data, options ) {
 
 	// Add code here for mouse clicks/touches over a bead
 
-    if(PS.color(x,y) == PS.COLOR_GRAY_DARK){
-        if(within_field(x, y)){
-            for(var i=0; i<10; i += 1){
-                for(var j=0; j<10; j += 1){
-                    p_view[i][j] = PS.color(i + field_x, j+field_y);
-                }
-            }
-            s_select = null;
+    if(player === null)
+    {
+        K.register("p1", "p", function(r)
+        {
+           if(r.error)
+           {//if p1 already registered, register p2 and join p1's game
+               player = "p2";
+               K.register("p2", "p", function(r)
+               {
+                   K.submitInput("g", "JOIN", function(r)
+                   {
+                       step = "wait";
+                       K.listenUpdates("g", processUpdates);
+                       PS.statusText("Player 2 (Member) Registered");
+                   });
+               });
+           }
+            else
+           { //otherwise start game
+               player = "p1";
+               K.startGroup("g", "p", function(r)
+               {
+                   step = "wait";
+                   K.listenInputs("g", processInputs);
+                   PS.statusText("Player 1 (Host) Registered");
+               });
+           }
+        });
+    }
+    else if(step === "setup")
+    {
+        if(selected_piece === null && data.index !== undefined && own_ships[data.index].color === PS.COLOR_GRAY_DARK)
+        {//if unplaced ship click and no ship selected
+            selected_piece = data.index;
         }
-        else{
-            s_select = data;
-            flood(x, y, PS.COLOR_GRAY_DARK, PS.COLOR_GRAY_LIGHT);
+        else if(selected_piece !== null && valid_placement(x, y, own_ships[selected_piece]))
+        {//if unplaced ship selected and current location is a valid spot to put it
+            own_ships[selected_piece].color = PS.COLOR_GRAY_LIGHT; //change color to indicate ship has been placed
+            selected_piece = null; //unselect ship
+            if(++num_placed === 5)
+            {
+                if(player === "p1")
+                {
+                    K.submitUpdates("g", ["p1_ships", "p1_ready"], [own_ships, true], function(r){}, [false, true]);
+                    if(other_setup)
+                    {
+                        step = "move";
+                        PS.statusText("It is your turn");
+                        view_own_ships = false;
+                        draw_board();
+                    }
+                    else
+                    {
+                        step = "wait";
+                        PS.statusText("Waiting for opponent to finish setting up");
+                    }
+                }
+                else
+                {
+                    K.submitInput("g", {setup: own_ships}, function(r){});
+                    step = "wait";
+                    if(other_setup)
+                    {
+                        PS.statusText("Opponent's turn");
+                        view_own_ships = false;
+                        draw_board();
+                    }
+                    else
+                    {
+                        PS.statusText("Waiting for opponent to finish setting up");
+                    }
+                }
+                setup_done = true;
+            }
+            update_data();
+            draw_board();
         }
     }
-
-    if(PS.color(x, y) == PS.COLOR_GREEN){
-        PS.statusInput("Put your taunt here.", function(taunt){
-            PS.statusText(taunt);
-        });
+    else if (step === "move")
+    {
+        if(on_board(x, y) && hitboard[x - board_x][y - board_y] === undefined)
+        {
+            PS.statusText("Attack position chosen");
+            if(player === "p1")
+            {
+                var hit = false;
+                for(var i = 0; i < opp_ships.length; i++)
+                {
+                    if(x === opp_ships[i].pos.x)
+                    {
+                        for(var j = 0; j < opp_ships[i].hits.length; j++)
+                        {
+                            if(y === opp_ships[i].pos.y + j)
+                            {
+                                opp_ships[i].hits[j] = true;
+                                hit = true;
+                            }
+                        }
+                    }
+                    else if(y === opp_ships[i].pos.y)
+                    {
+                        for(var j = 0; j < opp_ships[i].hits.length; j++)
+                        {
+                            if(x === opp_ships[i].pos.x + j)
+                            {
+                                opp_ships[i].hits[j] = true;
+                                hit = true;
+                            }
+                        }
+                    }
+                }
+                hitboard[x - board_x][y - board_y] = hit;
+                K.submitUpdates("g", "host move", {x: x, y: y, hit: hit}, function(r){}, true, "p2");
+                step = "wait";
+                PS.statusText("Opponent's turn");
+            }
+            else
+            {
+                K.submitInput("g", {x: x, y: y}, function(){});
+                step = "wait";
+                PS.statusText ("validating move");
+            }
+            draw_board();
+        }
     }
 };
 
@@ -218,22 +402,10 @@ PS.release = function( x, y, data, options ) {
 
 PS.enter = function( x, y, data, options ) {
 	"use strict";
-    if(within_field(x, y)){
-        for(var i=0; i<10; i += 1){
-            for(var j=0; j<10; j += 1){
-                PS.color((i + field_x), (j+field_y), p_view[i][j]);
-            }
-        }
-        if(s_select){
-            if(check_collide(x, y, s_select.I, s_select.H, 0)){
-                if(s_select.I == "vert"){
-                    PS.applyRect(x, y, 1, s_select.H, PS.color, PS.COLOR_GRAY_DARK);
-                }
-                else{
-                    PS.applyRect(x, y, s_select.H, 1, PS.color, PS.COLOR_GRAY_DARK);
-                }
-            }
-        }
+    if(selected_piece !== null)
+    {
+        own_ships[selected_piece].pos = {x: x, y: y};
+        draw_board();
     }
 
 	// Uncomment the following line to inspect parameters
@@ -298,14 +470,15 @@ PS.keyDown = function( key, shift, ctrl, options ) {
 //	}
 
 	// Add code here for when a key is pressed
-    if(key == 116){
-        if(s_select){
-            if(s_select.I == "vert"){
-                s_select.I = "horiz";
+    if(key == 116){ //rotate with t
+        if(selected_piece !== null){
+            if(own_ships[selected_piece].orientation == "vert"){
+                own_ships[selected_piece].orientation = "hor";
             }
             else{
-                s_select.I = "vert"
+                own_ships[selected_piece].orientation = "vert";
             }
+            draw_board();
         }
     }
 };
